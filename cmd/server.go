1e44383ef44faa1979fb77bb1df4c1b6403365d2
mariammahrous23 <mariammahrous23@gmail.com>
2024-07-02 12:25:55 +0300

    feat : http server + endpoint implemention

---
+++ cmd/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	dateAndTime "github.com/codescalersinternships/DateTime-Server-Mariam_Mahrous/pkg"
+	"net/http"
+)
+
+func main() {
+	
+	http.HandleFunc(
+		"/datetime",
+		dateAndTime.GetDateAndTime,
+	)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("an error occured")
+	}
+}
